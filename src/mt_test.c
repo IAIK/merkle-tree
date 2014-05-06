43d37f915623aad1af45569bee01f08872768851
Daniel Hein <daniel.hein@iaik.tugraz.at>
2014-05-06 11:26:50 +0200

    renamed Merkle Tree test file

--- src/mt_test.c
+++ src/mt_test.c
@@ -6,19 +6,24 @@
  */
 
 #include "config.h"
+#include "merkletree.h"
 #include "mt_arr_list.h"
 
-#define TEST_VALUES 17
+#define D_TEST_VALUES 17
 
-int main() {
-  uint8_t test_values[TEST_VALUES][HASH_LENGTH];
-  for (uint32_t i = 0; i < TEST_VALUES; ++i) {
+static uint8_t test_values[D_TEST_VALUES][D_HASH_LENGTH];
+
+static void mt_test_init() {
+  for (uint32_t i = 0; i < D_TEST_VALUES; ++i) {
     for (uint32_t c = 0; c < HASH_LENGTH; ++c) {
       test_values[i][c] = i;
     }
   }
+}
+
+void mt_test_mt_al() {
   mt_al_t *list = mt_al_create();
-  for (uint32_t i = 0; i < TEST_VALUES; ++i) {
+  for (uint32_t i = 0; i < D_TEST_VALUES; ++i) {
     mt_al_add(list, test_values[i]);
     mt_al_print(list);
   }
@@ -28,7 +33,7 @@ int main() {
   mt_al_print(list);
   mt_al_truncate(list, 5);
   mt_al_print(list);
-  for (uint32_t i = TEST_VALUES-4; i < TEST_VALUES; ++i) {
+  for (uint32_t i = D_TEST_VALUES - 4; i < D_TEST_VALUES; ++i) {
     mt_al_add(list, test_values[i]);
     mt_al_print(list);
   }
@@ -36,6 +41,21 @@ int main() {
   mt_al_print(list);
   mt_al_truncate(list, 0);
   mt_al_print(list);
-	return 0;
+}
+
+void mt_test_tree() {
+  mt_t *mt = mt_create();
+  mt_print(mt);
+  for (uint32_t i = 0; i < 5; ++i) {
+    mt_add_block(mt, test_values[i],i);
+    mt_print(mt);
+  }
+  mt_delete(mt);
+}
+
+int main() {
+  mt_test_init();
+  mt_test_tree();
+  return 0;
 }
 
