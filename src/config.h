1f8ee06805ff26de43512897d67d29aa6421ac07
Daniel Hein <daniel.hein@iaik.tugraz.at>
2014-05-09 16:07:30 +0200

    add more docu for config.h

--- src/config.h
+++ src/config.h
@@ -13,9 +13,9 @@
 #include <stdint.h>
 
 #define HASH_LENGTH                      32u  /*!< The length of the hash function output in bytes */
-#define TREE_LEVELS                      19u  /*!< The number of levels in the tree minus one, because we don't count the root node */
-#define BLOCK_SIZE                     4096u  /*!< Block size */
-#define MAX_FILE_SIZE   UINT32_C(2147483647)
+#define TREE_LEVELS                      20u  /*!< The number of levels in the tree */
+#define BLOCK_SIZE                     4096u  /*!< The size of a single block in the file */
+#define MAX_FILE_SIZE   UINT32_C(2147483647)  /*!< The maximum size of a file */
 #define MT_AL_MAX_ELEMS (MAX_FILE_SIZE / BLOCK_SIZE) /*!< The maximum number of elements in a Merkle Tree array list. Essential for integer overflow protection! */
 
 #endif /* CONFIG_H_ */
