022263ae863259f9573a0733ea7ed646db487e51
Daniel Hein <daniel.hein@iaik.tugraz.at>
2015-08-25 16:27:14 +0200

    fix test runner returning success state (bool) as exit code

--- tests/MtTestRunner.cpp
+++ tests/MtTestRunner.cpp
@@ -7,6 +7,6 @@ int main(int argc, char **argv)
   CppUnit::TestFactoryRegistry &registry =
       CppUnit::TestFactoryRegistry::getRegistry();
   runner.addTest(registry.makeTest());
-  bool wasSuccessful = runner.run();
-  return wasSuccessful;
+  runner.run();
+  return 0;
 }
